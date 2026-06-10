5122cb30cc754ac446fdf5434cf71e09dcee9276
Muhammad Zain Ul Abideen <mzainulabideen918@gmail.com>
2026-06-10 14:46:23 +0500

    chore(init): setup ASP.NET MVC and Web API projects

---
+++ HealthcareCRM.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HealthcareCRM.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
